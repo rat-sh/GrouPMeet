790bd6cccf33c4347d3db0fcbb14e4476ee9f4c0
rat-sh <cserathighosh@gmail.com>
2026-05-04 16:25:00 +0530

    feat: integrate socket.io for real-time messaging and update Chat model schema

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,15 @@
 import app from "./src/app";
 import { connectDB } from "./src/config/database";
+import { createServer } from "http";
+import { initializeSocket } from "./src/utils/socket";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
+
+const httpServer = createServer(app)
+initializeSocket(httpServer)
 
 connectDB().then(() => {
-    app.listen(PORT, () => {
+    httpServer.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
 })
--- backend/src/models/Chat.ts
+++ backend/src/models/Chat.ts
@@ -2,7 +2,7 @@ import mongoose, { Schema, type Document } from "mongoose";
 
 export interface IChat extends Document {
     participants: mongoose.Types.ObjectId[];
-    lastMessage: mongoose.Types.ObjectId[];
+    lastMessage?: mongoose.Types.ObjectId;
     lastMessageAt?: Date;
     createdAt: Date;
     updatedAt: Date;
@@ -17,13 +17,11 @@ const ChatSchema = new Schema<IChat>(
                 required: true
             }
         ],
-        lastMessage: [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "Message",
-                default: null
-            }
-        ],
+        lastMessage: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Message",
+            default: null
+        },
         lastMessageAt: {
             type: Date,
             default: Date.now(),
